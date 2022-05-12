ecb4181dcbb282a508d8a7b74feeda9ca8f79eb3
harumi <firmansyah720.fs@gmail.com>
2022-05-12 11:45:15 +0700

    First Version

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+let emailReceiver = 'firmansyah720.fs@gmail.com';
+
+function submitForm() {
+    let formObject = {
+        name: document.getElementById('input-name').value,
+        email: document.getElementById('input-email').value,
+        phone: document.getElementById('input-phone').value,
+        subject: document.getElementById('input-subject').value,
+        message: document.getElementById('input-your-message').value,
+    };
+
+    // console.log(formObject);
+
+    let a = document.createElement('a');
+    a.href = `mailto:${emailReceiver}?subject=${formObject.subject}&body=Hello, my name ${formObject.name}, My Phone number is ${formObject.phone} ${formObject.subject}, ${formObject.message}`;
+    a.click();
+}
